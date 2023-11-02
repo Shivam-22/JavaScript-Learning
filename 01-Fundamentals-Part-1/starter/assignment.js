1043734f33c24aa90a725ac1ee4ec51a72e3ace8
Shivam_22 <shivam.deshmukh@nuance.com>
2023-11-02 15:14:00 +0530

    completed logical operator in js fundamental assignment.

--- 01-Fundamentals-Part-1/starter/assignment.js
+++ 01-Fundamentals-Part-1/starter/assignment.js
@@ -34,3 +34,25 @@ console.log(averagePopulation > population);
 
 let description =
   "Protugal is in Europe and its 11 million people speak portuguese";
+
+//// Lecture 5
+
+// const numNeighbours = prompt(
+//   "How many neighbour countries does your country have?"
+// );
+
+// if (Number(numNeighbours) === 1) {
+//   console.log("Only 1 border!");
+// } else if (Number(numNeighbours) > 1) {
+//   console.log("More than 1 border!");
+// } else {
+//   console.log("No borders");
+// }
+
+//// Lecture 6
+
+if (language === "English" && population < 50000000 && !isIsland) {
+  console.log(`You should live in ${country} 😊`);
+} else {
+  console.log(`${country} does not meet your criteria 😔`);
+}
