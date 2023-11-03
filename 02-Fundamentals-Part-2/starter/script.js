30c03bd0e3bf986e9f53d762fdb2c96855227815
Shivam_22 <shivam.deshmukh@nuance.com>
2023-11-04 00:42:15 +0530

    function fundamental is completed

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,5 @@
 // // Activating Strict Mode
-// "use strict";
+"use strict";
 
 // let hasDriversLicence = false;
 // const passTest = true;
@@ -11,3 +11,23 @@
 // const interface = "Audio";
 // const private = 543;
 // const if = 34;
+
+//// Functions
+
+function logger() {
+  console.log("My name is Shivam");
+}
+
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+  return juice;
+}
+
+const applejuice = fruitProcessor(5, 0);
+console.log(applejuice);
+console.log(fruitProcessor(5, 0));
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
