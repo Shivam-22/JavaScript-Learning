aadbeee3a0c1e826c43045c08a5df3e6051766ba
Shivam_22 <shivam.deshmukh@nuance.com>
2023-11-04 00:52:47 +0530

    function declaration and function expression is completed

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,5 +1,5 @@
 // // Activating Strict Mode
-"use strict";
+//"use strict";
 
 // let hasDriversLicence = false;
 // const passTest = true;
@@ -14,20 +14,36 @@
 
 //// Functions
 
-function logger() {
-  console.log("My name is Shivam");
-}
+// function logger() {
+//   console.log("My name is Shivam");
+// }
 
-logger();
+// logger();
 
-function fruitProcessor(apples, oranges) {
-  const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
-  return juice;
-}
+// function fruitProcessor(apples, oranges) {
+//   const juice = `Juice with ${apples} apples and ${oranges} oranges.`;
+//   return juice;
+// }
 
-const applejuice = fruitProcessor(5, 0);
-console.log(applejuice);
-console.log(fruitProcessor(5, 0));
+// const applejuice = fruitProcessor(5, 0);
+// console.log(applejuice);
+// console.log(fruitProcessor(5, 0));
 
-const appleOrangeJuice = fruitProcessor(2, 4);
-console.log(appleOrangeJuice);
+// const appleOrangeJuice = fruitProcessor(2, 4);
+// console.log(appleOrangeJuice);
+
+//// Function Declaration
+// function calcAge1(birthYear) {
+//   return 2037 - birthYear;
+// }
+
+// const age1 = calcAge1(1991);
+// console.log(age1);
+
+//// Function Expression OR Anonymous Function
+
+// const calcAge2 = function (birthYear) {
+//   return 2037 - birthYear;
+// };
+// const age2 = calcAge2(1991);
+// console.log(age2);
