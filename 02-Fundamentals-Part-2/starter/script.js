c846428cefaf8eaa8fe12245be8e96be8b0644e9
Shivam_22 <shivam.deshmukh@nuance.com>
2023-11-04 23:17:45 +0530

    object Methods completed.

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -153,46 +153,82 @@
 
 //// Objects
 //Object literal Syntax
-const shivam = {
-  firstName: "Shivam",
-  lastName: "Deshmukh",
-  age: 2037 - 1994,
-  job: "Software Engineer",
-  friends: ["Michael", "Steven", "Peter"],
-};
+// const shivam = {
+//   firstName: "Shivam",
+//   lastName: "Deshmukh",
+//   age: 2037 - 1994,
+//   job: "Software Engineer",
+//   friends: ["Michael", "Steven", "Peter"],
+// };
 
-console.log(shivam);
-console.log(shivam.lastName); // Dot operator
+// console.log(shivam);
+// console.log(shivam.lastName); // Dot operator
 
-console.log(shivam["firstName"]); // Bracket
-const nameKey = "Name";
+// console.log(shivam["firstName"]); // Bracket
+// const nameKey = "Name";
 
-console.log(shivam["first" + nameKey]);
-console.log(shivam["last" + nameKey]);
+// console.log(shivam["first" + nameKey]);
+// console.log(shivam["last" + nameKey]);
 
-const interestedIn = prompt(
-  "What do you want to know about shivem? Choose between firstName, lastName, age, job and friends"
-);
+// const interestedIn = prompt(
+//   "What do you want to know about shivem? Choose between firstName, lastName, age, job and friends"
+// );
 
-console.log(shivam.interestedIn); // undefinded
+// console.log(shivam.interestedIn); // undefinded
 
-if (shivam[interestedIn]) {
-  console.log(shivam[interestedIn]);
-} else {
-  console.log(
-    "Wrong request.! Choose between firstName, lastName, age, job and friends "
-  );
-}
+// if (shivam[interestedIn]) {
+//   console.log(shivam[interestedIn]);
+// } else {
+//   console.log(
+//     "Wrong request.! Choose between firstName, lastName, age, job and friends "
+//   );
+// }
 
 // Dot notation to add new value to object
 
-shivam.location = "India";
-shivam["github"] = "shivam-22";
-console.log(shivam);
+// shivam.location = "India";
+// shivam["github"] = "shivam-22";
+// console.log(shivam);
 
 // Challenge
 // "Shivam has 3 friends and his best friends is called Michael"
 
-console.log(
-  `${shivam.firstName} has ${shivam.friends.length} friends, and his best friend is called ${shivam.friends[0]}`
-);
+// console.log(
+//   `${shivam.firstName} has ${shivam.friends.length} friends, and his best friend is called ${shivam.friends[0]}`
+// );
+
+//// Objects Methods
+
+const shivam = {
+  firstName: "Shivam",
+  lastName: "Deshmukh",
+  birthYear: 1991,
+  job: "Software Engineer",
+  friends: ["Michael", "Steven", "Peter"],
+  hasDriversLicense: true,
+  //   calcAge: function (birthYear) {
+  //     return 2037 - birthYear;
+  //   },
+
+  //   calcAge: function () {
+  //     console.log(this);
+  //     return 2037 - this.birthYear;
+  //   },
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+  getSummary: function () {
+    return `${this.firstName} is ${this.calcAge()} years old and work as ${
+      this.job
+    } and he has ${this.hasDriversLicense ? "a" : "no"} Driver's License`;
+  },
+};
+console.log(shivam.calcAge()); // Dot Operator
+//console.log(shivam["calcAge"]()); // Bracket Operator
+
+console.log(shivam.age); // It will calculate age once and then we will just retrieve  the age property
+console.log(shivam.age);
+console.log(shivam.age);
+console.log(shivam.getSummary());
