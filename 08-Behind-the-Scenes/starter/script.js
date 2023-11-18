f3abf43ca35b160c69d0bdd90f29cb16bd53c96f
Shivam_22 <shivam.deshmukh@nuance.com>
2023-11-19 00:02:35 +0530

    Primitive vs Object completed.

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -130,57 +130,56 @@ jonas.calcAge();
 
 const f = jonas.calcAge;
 console.log(f);
-*/
 
 const shivam = {
   firstName: 'shivam',
   year: 1991,
   calcAge: function () {
-    console.log(this);
-    console.log(2037 - this.year);
-  },
-  //! Arrow function does not get its own this keyword
-  greet: () => console.log(`Hey ${this.firstName}`),
+      console.log(this);
+      console.log(2037 - this.year);
+    },
+    //! Arrow function does not get its own this keyword
+    greet: () => console.log(`Hey ${this.firstName}`),
 };
 shivam.greet();
 
 const jonas = {
-  firstName: 'Jonas',
-  year: 1991,
-  calcAge: function () {
+    firstName: 'Jonas',
+    year: 1991,
+    calcAge: function () {
     console.log(this);
     console.log(2037 - this.year);
-
+    
     //! preseving this keyword - Solution 1
     // const self = this;
     // const isMillenial = function () {
-    //   ! this keyword will be undefined here
-    //   ! since its a regular a regular function call and its undefined.
-    //   //console.log(this);
-    //   //console.log(this.year >= 1981 && this.year <= 1996);
-    //   ! We preseve the this keyword as part of above storing this in self.
-    //   console.log(self);
-    //   console.log(self.year >= 1981 && self.year <= 1996);
-    // };
-
-    //! Solution 2
-    //! Arrow function uses the this keyword of its parent and in our case its Jonas object
-    const isMillenial = () => {
-      console.log(this);
-      console.log(this.year >= 1981 && this.year <= 1996);
-    };
-    isMillenial();
-  },
-  //! Arrow function does not get its own this keyword
-  greet: () => console.log(`Hey ${this.firstName}`),
+        //   ! this keyword will be undefined here
+        //   ! since its a regular a regular function call and its undefined.
+        //   //console.log(this);
+        //   //console.log(this.year >= 1981 && this.year <= 1996);
+        //   ! We preseve the this keyword as part of above storing this in self.
+        //   console.log(self);
+        //   console.log(self.year >= 1981 && self.year <= 1996);
+        // };
+        
+        //! Solution 2
+        //! Arrow function uses the this keyword of its parent and in our case its Jonas object
+        const isMillenial = () => {
+            console.log(this);
+            console.log(this.year >= 1981 && this.year <= 1996);
+        };
+        isMillenial();
+    },
+    //! Arrow function does not get its own this keyword
+    greet: () => console.log(`Hey ${this.firstName}`),
 };
 jonas.greet();
 jonas.calcAge();
 
 //argument keyword
 const addExpr = function (a, b) {
-  console.log(arguments);
-  return a + b;
+    console.log(arguments);
+    return a + b;
 };
 addExpr(2, 5);
 //? We can add more arguments, its definatly fine.
@@ -188,8 +187,72 @@ addExpr(2, 3, 4, 21, 14);
 
 //! argument keywords exist in regular function not in arrow function
 var addArrow = (a, b) => {
-  console.log(arguments);
-  return a + b;
+    console.log(arguments);
+    return a + b;
 };
 
 addArrow(2, 3);
+
+
+let age = 30;
+let oldAge = age;
+age = 31;
+
+console.log(age); //! 31
+console.log(oldAge); //! 30
+
+const me = {
+    name: 'Jonas',
+    age: 30,
+};
+
+const friend = me;
+
+friend.age = 27;
+
+console.log('Friend:', friend); //! both will have age as 27
+console.log('Me:', me);
+
+*/
+
+//! Primitive Types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName, oldLastName);
+
+//! Reference Type
+const jessica = {
+  firstName: 'Jessica',
+  lastName: ' Williams',
+  age: 27,
+};
+
+const marriedJessica = jessica;
+marriedJessica.lastName = 'David';
+
+console.log('Before Marriage:', jessica);
+console.log('After Marriage:', marriedJessica);
+
+//marriedJessica = {}; // Not allowed due to constant
+
+//! Coping Objects
+const jessica2 = {
+  firstName: 'Jessica',
+  lastName: ' Williams',
+  age: 27,
+  family: ['Alice', 'Bob'],
+};
+
+//! This is a shallow copy. it will only create copy upto 1 level
+const jessicaCopy = Object.assign({}, jessica2);
+jessicaCopy.lastName = 'Davis';
+
+console.log('Before Marriage:', jessica2);
+console.log('After Marriage:', jessicaCopy);
+
+jessicaCopy.family.push('Mary');
+jessicaCopy.family.push('John');
+
+console.log('Before Marriage:', jessica2); //! Both will have family of 4 members
+console.log('After Marriage:', jessicaCopy); //! Both will have family of 4 members
