77088c80f496f742e61b51e9cf8dfb2e260dd9a9
Shivam_22 <shivam.deshmukh@nuance.com>
2023-11-23 19:56:46 +0530

    rest pattern and parameters completed.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -38,6 +38,10 @@ const restaurant = {
       `Here is your delicous pasta with ${ing1}, ${ing2} and ${ing3}`
     );
   },
+  orderPizza: function (mainIngredient, ...otherIngredient) {
+    console.log(mainIngredient);
+    console.log(otherIngredient);
+  },
 };
 
 /*
@@ -167,4 +171,44 @@ console.log(a, b, c);
 //! Default Values for destructuring
 const [p = 1, q = 1, r = 1] = [8, 9];
 console.log(p, q, r);
+
+//! Rest operator
+//? Spread, because on RIGHT side of =
+const arr = [1, 2, ...[3, 4]];
+
+//? Rest, because on LEFT side of =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+
+console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+
+console.log(pizza, risotto, otherFood);
+
+// Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+console.log(weekdays);
+
+// 2) Functions
+
+const add = function (...numbers) {
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    sum += numbers[i];
+  }
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('mushrrom', 'Onion', 'olives', 'tomatos');
+
 */
