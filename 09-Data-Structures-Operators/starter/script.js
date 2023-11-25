f8bfe56c26a970fbcc25e20604172af48ace6ec5
Shivam_22 <shivam.deshmukh@nuance.com>
2023-11-26 00:57:14 +0530

    Commented code.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -780,11 +780,10 @@ planesInLine(5);
 planesInLine(3);
 planesInLine(12);
 
-*/
 
 // Data needed for a later exercise
 const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+'_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 console.log(flights.split('+'));
 
@@ -797,6 +796,8 @@ for (const flight of flights.split('+')) {
   } ${type.replaceAll('_', ' ')} ${getCode(from)} ${getCode(to)} ${time.replace(
     ':',
     'h'
-  )}`.padStart(36);
-  console.log(outputStr);
-}
+    )}`.padStart(36);
+    console.log(outputStr);
+  }
+  
+  */
