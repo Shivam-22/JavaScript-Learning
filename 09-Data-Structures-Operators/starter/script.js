0d61a87ccf612420bafa0bd98ee81bec6e500b2e
Shivam_22 <shivam.deshmukh@nuance.com>
2023-11-26 00:56:07 +0530

    string part 4 completed.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,9 +1,5 @@
 'use strict';
 
-// Data needed for a later exercise
-const flights =
-  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
-
 // Data needed for first part of the section
 const restaurant = {
   name: 'Classico Italiano',
@@ -714,7 +710,6 @@ const announcement =
 
 console.log(announcement.replace('door', 'gate'));
 console.log(announcement.replaceAll('door', 'gate'));
-*/
 
 //* Booleans
 
@@ -784,3 +779,24 @@ const planesInLine = function (n) {
 planesInLine(5);
 planesInLine(3);
 planesInLine(12);
+
+*/
+
+// Data needed for a later exercise
+const flights =
+  '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
+
+console.log(flights.split('+'));
+
+const getCode = str => str.slice(0, 3).toUpperCase();
+
+for (const flight of flights.split('+')) {
+  const [type, from, to, time] = flight.split(';');
+  const outputStr = `${
+    type.startsWith('_Delayed') ? '🔴' : ''
+  } ${type.replaceAll('_', ' ')} ${getCode(from)} ${getCode(to)} ${time.replace(
+    ':',
+    'h'
+  )}`.padStart(36);
+  console.log(outputStr);
+}
