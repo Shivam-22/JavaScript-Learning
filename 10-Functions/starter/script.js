926ee5f56919bcd095d83beef0b942332af99ed7
Shivam_22 <shivam.deshmukh@nuance.com>
2024-01-06 17:18:17 +0530

    Default Parameters

---
+++ 10-Functions/starter/script.js
@@ -0,0 +1,29 @@
+'use strict';
+
+//// Default Parameters
+// const bookings = [];
+
+// const createBooking = function (
+//   flightNum,
+//   numPassegers = 1,
+//   price = 199 * numPassegers
+// ) {
+//   //   // ES5
+//   //   numPassegers = numPassegers || 1;
+//   //   price = price || 199;
+
+//   //! ES6 - add in paramter
+
+//   const booking = {
+//     flightNum,
+//     numPassegers,
+//     price,
+//   };
+
+//   bookings.push(booking);
+// };
+// console.log(bookings);
+// createBooking('LH123');
+// createBooking('LH123', 2, 800);
+// createBooking('LH123', 2);
+// createBooking('LH123', 5);
