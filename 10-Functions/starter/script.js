337e20ce01065b5405ec377733c5fb086840b731
Shivam_22 <shivam.deshmukh@nuance.com>
2024-01-06 17:36:49 +0530

    Pass by value vs pass from reference completed

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -27,3 +27,32 @@
 // createBooking('LH123', 2, 800);
 // createBooking('LH123', 2);
 // createBooking('LH123', 5);
+
+//// Pass Arguments value vs reference
+
+// const flight = 'LH234';
+// const jonas = {
+//   name: 'Jonas Schmedtmann',
+//   passport: 4132456789545,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr. ' + passenger.name;
+
+//   if (passenger.passport === 4132456789545) {
+//     alert('Checked In');
+//   } else {
+//     alert('Wrong passport');
+//   }
+// };
+
+// checkIn(flight, jonas);
+// console.log(flight);
+// console.log(jonas);
+
+// const newPassport = function (person) {
+//   person.passport = Math.trunc(Math.random() * 100000000000);
+// };
+// newPassport(jonas);
+// checkIn(flight, jonas);
