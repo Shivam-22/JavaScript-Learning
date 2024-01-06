846c92477d774dccd66a55e21638ddbb066f9ae7
Shivam_22 <shivam.deshmukh@nuance.com>
2024-01-06 20:21:01 +0530

    the bind method completed

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -145,3 +145,47 @@ console.log(swiss);
 
 book.call(swiss, ...flightData);
 console.log(swiss);
+
+//! Bind method
+//book.call(euroWings, 23, 'Sarah Williams');
+
+const bookEW = book.bind(euroWings);
+const bookLH = book.bind(lufthansa);
+const bookLX = book.bind(swiss);
+
+bookEW(23, 'Steven Williams');
+console.log(euroWings);
+
+const bookEW23 = book.bind(euroWings, 23);
+bookEW23('Shivam Deshmukh');
+bookEW23('Martha Cooper');
+
+//! With Event Listners
+lufthansa.planes = 300;
+lufthansa.buyPlane = function () {
+  console.log(this);
+  this.planes++;
+  console.log(this.planes);
+};
+
+document
+  .querySelector('.buy')
+  .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+//! Partial application
+
+const addTax = (rate, value) => value + value * rate;
+console.log(addTax(0.1, 200));
+
+const addVAT = addTax.bind(null, 0.23);
+console.log(addVAT(100));
+
+const addTaxRate = function (rate) {
+  return function (value) {
+    return value + value * rate;
+  };
+};
+
+const addVAT2 = addTaxRate(0.23);
+console.log(addVAT2(100));
+console.log(addVAT2(23));
