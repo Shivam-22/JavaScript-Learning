5231bca66aa38760a5c119f53c2825aef3600c06
Shivam_22 <shivam.deshmukh@nuance.com>
2024-01-06 18:00:41 +0530

    callback function completed

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -56,3 +56,24 @@
 // };
 // newPassport(jonas);
 // checkIn(flight, jonas);
+
+//// Higher Order Functions
+
+const oneWord = function (str) {
+  return str.replace(/ /g, '').toLowerCase();
+};
+
+const upperFirstWord = function (str) {
+  const [first, ...others] = str.split(' ');
+  return [first.toUpperCase(), ...others].join(' ');
+};
+
+const transformer = function (str, fn) {
+  console.log(`Original Str: ${str}`);
+  console.log(`Transform string: ${fn(str)}`);
+
+  console.log(`Transformed by: ${fn.name}`);
+};
+
+transformer('JavaScript is the best', upperFirstWord);
+transformer('JavaScript is the best', oneWord);
